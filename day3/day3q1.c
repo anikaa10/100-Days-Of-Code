bf187f81279a0a2fe5558d6900ca184d971f4156
Anika Gupta <anikag630@gmail.com>
2026-09-16 06:28:13 +0000

    Day3 ques 1

---
+++ day3/day3q1.c
@@ -0,0 +1,57 @@
+Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int celsius, fahrenheit;
+
+    scanf("%d", &celsius);
+
+    fahrenheit = (celsius * 9 / 5) + 32;
+
+    printf("Fahrenheit=%d", fahrenheit);
+
+    return 0;
+}
